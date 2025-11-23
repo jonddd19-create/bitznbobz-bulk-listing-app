62a8a03da8e9d271722903bfb96158fefc7e9a79
jonddd19-create <jondodd19@gmail.com>
2025-11-23 23:24:14 +0000

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-export async function enrichUrls({ urls, includeZip }) {
-  const res = await fetch("/.netlify/functions/generateAll", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ urls, includeZip })
-  });
-  if (!res.ok) throw new Error(await res.text());
-  return res.json();
-}
